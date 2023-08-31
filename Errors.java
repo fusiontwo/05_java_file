0c31e08d7074a7a2a10a0748800876fce4c696a9
최예름 <101881124+fusiontwo@users.noreply.github.com>
2023-08-31 12:45:55 +0900

    Add files via upload

---
+++ Errors.java
@@ -0,0 +1,12 @@
+//package week01.jhchi;==> 패키지가 서로 달라서 오류 발생 ==> 패키지 이름을
+package week01.jhchi;
+
+// public class errors : 파일 이름과 클래스 이름이 서로 달라 오류 발생
+public class Errors
+{
+  public Static main(string[] args) {
+    System.out.println(“이 프로그램은 많은 오류를 가지고 있다.”);
+    System.0ut.println(“그러나 프로그램이 이 문장을 출력된다면”)
+    System.out.Println(“모든 오류를 고친 것이다.);
+  }
+}
---
+++ TestMain.java
@@ -0,0 +1,12 @@
+package week01.example01;
+
+public class TestMain {
+	
+	enum Weekday {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("202112439 최예름\n");
+		System.out.println("202112439");
+	}
+}
